0e70e705cc9ba08a13f4f875d06d84ab9379f03c
Cipher <dipanshusaini69@gmail.com>
2023-08-28 21:39:23 +0530

    added the auth function and set it to false added the new mongodb string for cloud atlas

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,10 @@
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.SECRET_KEY;
+const authorisation = process.env.REQUIRES_AUTH;;
 
 module.exports = function (req, res, next) {
+  if(!authorisation)next();
+  
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access Denied. No token providaldkfkjsed...");
 
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 
 const logger = require("./startup/logger");
 const express = require("express");
+const cors = require('cors');
 const app = express();
+
+app.use(cors()); // for cross origin requests
 
 require("./startup/logging")();
 require("./startup/routes")(app);
